74fdf9ef9a9cb272d38a6bbd09de068561b91764
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-08-03 16:54:53 -0600

    Update NavBar.tsx
    working navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,14 +3,46 @@
 // ============================================================================
 // Display's page title and links to Discover Animals and Plan Your Visit
 // ----------------------------------------------------------------------------
+'use client'
+import Link from "next/link"
+import {usePathname} from "next/navigation";
+import React from 'react'
+
+const navLinks=[
+    
+    {href: '/animals', label: 'Animals'},
+    {href: '/habitats', label: 'Habitats'},
+    {href: '/aboutus', label: 'About Us'},
+]
 
 export default function Navbar() {
+        const pathname = usePathname()
+    console.log("Pathname",pathname)
   return (
-    <header className="bg-white border-b border-gray-200 px-6 py-4">
-      <div className="max-w-2xl mx-auto">
-        <h1 className="text-lg font-bold text-gray-900">Acme Inc · Staff Directory</h1>
-        <p className="text-xs text-gray-400">CPRG 306 · Week 10 · Supabase Databases</p>
+    <nav className="bg-red-50 border-b border-gray-200 px-6 py-4 ">
+      <div className=" flex flex-row ">
+        <div className="w-1/3">
+            <Link key='/' href='/' className="text-4xl font-bold text-green-900 text-start">BestiaryU</Link>
+        
+        </div>
+        <div className=" w-1/3 flex flex-row justify-center pt-3 gap-5">
+        {
+            navLinks.map((link)=>{
+                const isActive = pathname ===link.href
+                return(
+                    <Link key={link.href}
+                    href={link.href}
+                    className = {['text-lg text-center border-b-2',
+                        isActive ? "text-green-900 font-bold border-green-900" : "text-slate-400 font-normal border-transparent hover:text-slate-200",
+
+                    ].join(' ')}>{link.label}</Link>
+                )
+            })
+        }
+   
+        </div>
+
       </div>
-    </header>
+    </nav>
   );
 }
