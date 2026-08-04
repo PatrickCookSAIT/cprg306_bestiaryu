ba74a4256892b45fd32fd586a87d2f53d40e20a2
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-08-04 16:49:49 -0600

    animals page

--- app/animals/page.tsx
+++ app/animals/page.tsx
@@ -1,14 +1,44 @@
 import React from 'react'
+import { animals } from "../../lib/data";
+import Link from "next/link";
+
+import ViewAnimalCard from '@/components/ViewAnimalCard';
 
 const page = () => {
   return (
-    <div className="bg-red-50">
-      <div className="flex flex-col w-[90%] lg:w-1/2 mx-10 mt-10">
-      <h2 className="text-xa text-orange-800 font-semibold mb-5">ENRICHED DISCOVERY</h2>
-      <h1 className="text-4xl lg:text-6xl text-green-950 font-serif font-bold">Meet the Denizens of Our Domain</h1>
-      <p className="mt-5 text-sm text-green-900">Explore our collection of extraordinary wildlife, from the driest of desert dwellers to the creatures of summit and sky.</p>
+    <div className="bg-red-50 w-full flex flex-col">
+      <div className="flex flex-col w-[90%] lg:w-1/2 px-10 mt-10">
+        <h2 className="text-xa text-orange-800 font-semibold mb-5">ENRICHED DISCOVERY</h2>
+        <h1 className="text-4xl lg:text-6xl text-green-950 font-serif font-bold">Meet the Denizens of Our Domain</h1>
+        <p className="mt-5 text-sm text-green-900">Explore our collection of extraordinary wildlife, from the driest of desert dwellers to the creatures of summit and sky.</p>
       </div>
+      <div className="w-[90%] px-10 border border-gray-200 rounded-lg self-center mt-10 lg:mt-20 flex flex-row justify-between">
+        <div>
+            <h3>All Habitats</h3>
+        </div>
+        <div>
+            <h3>Conservation Status</h3>
+        </div>
+        <div>
+            <h3>Animal Class</h3>
+        </div>
+       </div>
+       <div className="grid grid-cols-1 lg:grid-cols-4  lg:justify-around lg:mx-10 max-w-full">
+    {animals.slice(0, 8).map((animal) => (
+  <ViewAnimalCard
+    key={animal.id}
+    imageUri={animal.imageUri}
+    species={animal.species}
+    habitat={animal.habitat}
+    blurb={animal.blurb}
+    conservationStatus={animal.conservationStatus}
+    class={animal.class}
+  />
+  
+))}
+</div>
     </div>
+    
   )
 }
 
---
+++ components/ViewAnimalCard.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Image from "next/image";
+import ConservationStatusCard from './ConservationStatusCard';
+
+type ViewAnimalCardProps = {
+  imageUri: string;
+  habitat: string;
+  species: string;
+  blurb: string;
+  conservationStatus: string
+  class: string
+};
+
+const ViewAnimalCard = ({
+  imageUri,
+  habitat,
+  species,
+  blurb,
+  conservationStatus,
+  class
+}: ViewAnimalCardProps) => {
+  return (
+    <div className="bg-white  h-120 max-w-full w-[90%] ml-4 lg:w-70 rounded-2xl mt-10 flex flex-col lg:my-10">
+        <div className="w-full p-4 h-80">
+            
+          <Image
+            src={imageUri}
+            alt={species}
+            width={480}
+            height={300}
+            sizes="(max-width: 768px) 100vw, 33vw"
+            className="w-full h-full object-cover"
+          />
+          <div className="flex flex-col">
+            <h2 className="text-orange-500 mt-7 text-sm font-semibold">{habitat.toUpperCase()}</h2>
+            <h1 className="text-green-900 font-bold text-2xl">{species}</h1>
+            <div className="h-10">
+              <p className="text-green-800 text-xs">{blurb}</p>
+            </div>
+            <div className="justify-end items-end align-bottom">
+              <ConservationStatusCard conservationStatus={conservationStatus} />
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAnimalCard
