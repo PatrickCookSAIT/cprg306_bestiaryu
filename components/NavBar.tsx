31930af90bf8f6562c1945d8113d5f0d37900746
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-08-07 13:19:49 -0600

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,7 +11,7 @@ import React from 'react'
 const navLinks=[
     
     {href: '/animals', label: 'Animals'},
-    {href: '/map', label: 'Map'},
+    {href: '/habitats', label: 'Habitats'},
     {href: '/schedule', label: 'Schedule'},
     {href: '/about', label: 'About Us'},
 ]
