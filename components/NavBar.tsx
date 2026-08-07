f4b9e1bf651d63f6e4e4e4435edf90d3740b1155
V-Weaver <ash.v.weaver@proton.me>
2026-08-07 12:25:32 -0600

    added links to map

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,7 +11,7 @@ import React from 'react'
 const navLinks=[
     
     {href: '/animals', label: 'Animals'},
-    {href: '/habitats', label: 'Habitats'},
+    {href: '/map', label: 'Map'},
     {href: '/schedule', label: 'Schedule'},
     {href: '/about', label: 'About Us'},
 ]
